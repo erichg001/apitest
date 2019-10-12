588375a55012cc231ae5d852c19ff5a50232e678
erichg001 <eric_hg@sina.com>
2019-10-12 17:28:33 +0800

    add new cases

--- src/main/java/com/bitnei/apitest/testcases/saas/YiQiCardAdd.java
+++ src/main/java/com/bitnei/apitest/testcases/saas/YiQiCardAdd.java
@@ -3,31 +3,25 @@ package com.bitnei.apitest.testcases.saas;
 import java.io.IOException;
 import java.util.HashMap;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-import com.bitnei.apitest.tool.DiffMethod;
 import com.bitnei.apitest.utils.ExcelReader;
 import com.bitnei.apitest.utils.ExcleUtil;
 import com.bitnei.apitest.utils.RestClient;
 
-import net.sf.json.JSONObject;
-
 /** 
 * @author 作者 eric_hg
-* @version 创建时间：2019年9月27日 下午4:26:47 
-* 类说明
-* 新建一汽数据集，通过excel读取参数
+* @version 创建时间：2019年9月29日 下午4:09:55 
+* 类说明 
+* 新增一汽报表，一个数据集下新建多个维护的报表，筛选条件为所有维度，通过excel读取参数
 */
-public class YiQiDataSet {
-
+public class YiQiCardAdd {
 	String host;
 	String url;
 	RestClient restClient;
@@ -43,7 +37,7 @@ public class YiQiDataSet {
 	public void setUp(String hostsaas,String pathroad) {
 		host = hostsaas;	
 		String ExcelFilePath= pathroad ;
-        String sheetName="yiqidata";
+        String sheetName="yiqireport";
         ex = new ExcelReader(ExcelFilePath, sheetName);
 		//设置cookie		
 		try {
@@ -51,13 +45,13 @@ public class YiQiDataSet {
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
-			e.printStackTrace();
+			e.printStackTrace(); 
 		}	
 		       
 	}
 	
-	@Test(description="yiqi数据集新增",priority =0,dataProvider="dp")
-	public void AddDataSet(String desc,String url,String paras,String result) throws ClientProtocolException, IOException {
+	@Test(description="yiqi报表新增",priority =0,dataProvider="dp")
+	public void CardAddData(String desc,String url,String paras,String result) throws ClientProtocolException, IOException {
 		restClient = new RestClient();
 		//DiffMethod diffMethod = new DiffMethod();
 		//DiffMethodByExcept diffMethod = new DiffMethodByExcept();
@@ -71,9 +65,9 @@ public class YiQiDataSet {
 		System.out.println("url----"+url);
 		System.out.println("paras----"+paras);
 		closeableHttpResponse = restClient.post(url, paras, headermap);		
-		HttpEntity entity = closeableHttpResponse.getEntity();
-		String str = EntityUtils.toString(entity, "utf-8");
-		System.out.println("source-----"+str);
+//		HttpEntity entity = closeableHttpResponse.getEntity();
+//		String str = EntityUtils.toString(entity, "utf-8");
+		//System.out.println("source-----"+str);
 		//System.out.println("except-----"+result);
 		//System.out.println("getStatusLine"+closeableHttpResponse.getStatusLine().getStatusCode());
 		status = closeableHttpResponse.getStatusLine().getStatusCode();
@@ -89,4 +83,5 @@ public class YiQiDataSet {
      Object[][] sheetData = ex.getSheetDataByPD();
         return  sheetData ;
     }
+
 }
