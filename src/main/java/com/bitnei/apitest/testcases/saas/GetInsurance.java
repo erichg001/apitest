8a5c984785501790a5b4f7e04a267f2a526e36c6
erichg001 <eric_hg@sina.com>
2020-02-11 17:10:05 +0800

    add testcases

--- src/main/java/com/bitnei/apitest/testcases/saas/GetInsurance.java
+++ src/main/java/com/bitnei/apitest/testcases/saas/GetInsurance.java
@@ -21,31 +21,30 @@ import com.bitnei.apitest.utils.RestClient;
 import net.sf.json.JSONObject;
 
 /** 
-* @author 作者 eric_hg
-* @version 创建时间：2019年12月11日 下午4:39:39 
+* @author 作者 hangang
+* @version 创建时间：2020年2月11日 上午11:04:14 
 * 类说明 
 */
-public class ApiTestPost {
+public class GetInsurance {
+
 	String host;
 	String url;
 	RestClient restClient;
 	CloseableHttpResponse closeableHttpResponse;
+	//GetCookie getCookisaas = new GetCookie();
 	GetApiToken getToken = new GetApiToken ();
 	String token = "";
 	ExcelReader ex ;
     ExcleUtil excleUtil;
     int status;
-    SaasGetSec saasGetSec = new SaasGetSec();
-    String[] authorization = null;
 		
 	@Parameters({"hostsaas","pathroad"})
 	@BeforeClass
 	public void setUp(String hostsaas,String pathroad) throws InterruptedException {
 		host = hostsaas;	
 		String ExcelFilePath= pathroad ;
-        String sheetName="apipost";
+        String sheetName="平安保险";
         ex = new ExcelReader(ExcelFilePath, sheetName);
-        //authorization = saasGetSec.GetSec();
 		//设置cookie	
 		try {
 			 token = getToken.ApiToken();
@@ -57,32 +56,39 @@ public class ApiTestPost {
 		       
 	}
 	
-	@Test(description="api返回验证",priority =0,dataProvider="dp")
-	public void ApiTest(String desc,String url,String paras,String result) throws ClientProtocolException, IOException {
+	@Test(description="openapi接口",priority =0,dataProvider="dp")
+	public void Insurance(String desc,String url,String paras,String signature,String timestamp,String result) throws ClientProtocolException, IOException {
 		restClient = new RestClient();
 		DiffMethod diffMethod = new DiffMethod();
+		//DiffMethodByExcept diffMethod = new DiffMethodByExcept();
 		//准备请求头信息
 		HashMap<String,String> headermap = new HashMap<String,String>();
 		headermap.put("Content-Type", "application/json"); //这个在postman中可以查询到
 		headermap.put("Authorization","bearer "+token);
-//		headermap.put("pub-sec",authorization[1]);
-//		headermap.put("sg-sec",authorization[2]);
-//		headermap.put("to-sec",authorization[0]);
+		headermap.put("signature", signature);
+		headermap.put("timestamp", timestamp);
 		//入参设置
 		url= host+url+paras;
 		//调用接口	
-		//System.out.println("url----"+url);
-		//System.out.println("paras----"+paras);
-		closeableHttpResponse = restClient.post(url, headermap);		
+		System.out.println("url----"+url);
+		System.out.println("paras----"+paras);
+		closeableHttpResponse = restClient.get(url, headermap);		
 		HttpEntity entity = closeableHttpResponse.getEntity();
 		String str = EntityUtils.toString(entity, "utf-8");
-		System.out.println("source-----"+str);
-		System.out.println("except-----"+result);
-		System.out.println("getStatusLine"+closeableHttpResponse.getStatusLine().getStatusCode());
+		//System.out.println("source-----"+str);
+		//System.out.println("except-----"+result);
+		//System.out.println("getStatusLine"+closeableHttpResponse.getStatusLine().getStatusCode());
+//		status = closeableHttpResponse.getStatusLine().getStatusCode();
+//		Assert.assertEquals(status, 200);
 		JSONObject lastobject = new JSONObject();
 		lastobject = diffMethod.diffFormatJson(JSONObject.fromObject(str),JSONObject.fromObject(result));
 		System.out.println(lastobject.toString());
 		Assert.assertEquals(lastobject.toString(), "{}");
+//		JSONObject jsonobject = JSONObject.fromObject(str);		
+//		System.out.println("jsonobject==========="+jsonobject);
+//		String code =  jsonobject.get("code").toString();
+//		System.out.println("code==========="+code);
+//		Assert.assertEquals(code, result);
 	}
 	
 	@DataProvider
